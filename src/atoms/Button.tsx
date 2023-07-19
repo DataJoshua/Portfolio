5f5c232caa435c68c634b76efaa443e6dc2e9664
Joshua Rodriguez <joshua.rodriguez@flatstack.com>
2023-07-19 12:07:19 +0400

    added responsive design for HOme

--- src/atoms/Button.tsx
+++ src/atoms/Button.tsx
@@ -1,10 +1,12 @@
 import { ButtonProp } from "../types/AtomTypes";
+import { motion } from "framer-motion";
 
 const Button = ({handleOnClick, label} : ButtonProp)=> {
-  return <button 
+  return <motion.button 
+    whileHover={{scale: 1.2}}
     onClick={(e : React.MouseEvent<HTMLButtonElement, MouseEvent>)=> handleOnClick(e)}
-    className="bg-[#1939ff] text-2xl py-2"
-  >{label}</button>
+    className="bg-[#1939ff] text-2xl py-2" 
+  >{label}</motion.button>
 }
 
 export default Button;
--- src/templates/Footer.tsx
+++ src/templates/Footer.tsx
@@ -1,11 +1,7 @@
 const Footer = ()=> {
   return (
     <>
-      <div className="bg-slate-900 grid grid-cols-3 py-8 px-8 gap-5">
-        <div className="">
-          <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, excepturi?</h1>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet deserunt nostrum molestias possimus blanditiis rem unde minima fugit! Quod, maxime.</p>
-        </div>
+      <div className="bg-slate-900 px-[15%] grid-cols-1 sm:grid grid-cols-2 py-8 px-8 gap-5">
         <div className="">
           <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, excepturi?</h1>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet deserunt nostrum molestias possimus blanditiis rem unde minima fugit! Quod, maxime.</p>
