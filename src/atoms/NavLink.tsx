07d98e739e11cbe9173206cde897d23a7eefff08
Joshua Rodriguez <joshua.rodriguez@flatstack.com>
2023-07-29 00:05:44 +0400

    svgs added to footer

--- src/atoms/NavLink.tsx
+++ src/atoms/NavLink.tsx
@@ -6,7 +6,7 @@ const NavLink = ({ path , label, handleOnClick} : NavLinkProps) =>  {
   
   return <motion.div 
       onClick={()=> handleOnClick()}
-      className="text-3xl hover:underline hover:cursor-pointer"
+      className="text-xl sm:text-3xl hover:underline hover:cursor-pointer"
       whileHover={{scale: 1.2}}
     ><Link to={path}>{label}</Link></motion.div>
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,7 @@ const Home = ()=> {
     <FrameContainer>
       <div className="flex justify-center items-center">
         <div className="text-end lg:text-start max-w-xs lg:max-w-lg flex-col flex gap-3 lg:items-start items-end">
-          <h1 className="text-3xl lg:text-6xl">Web Development without barreers</h1>
+          <h1 className="text text-2xl lg:text-6xl">Web Development without barreers</h1>
           <div className="flex items-center justify-end lg:justify-start">
             <Button label="Hire Us" handleOnClick={()=>{}}/>          
             <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
